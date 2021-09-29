40b72c33aebbf89e19e0fa149533a0f01d1b51b3
Harry French <harry.french@moonforge.com>
2021-09-29 20:12:10 +0100

    Working!

---
+++ LinearCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LinearCalculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(@"Welcome to the Linear Calculator
+
+                    No Precedence, just the operators you demand, when you want them
+
+                    Specify the number and it's following operator + - * /,
+                    Specify the = operator to finish
+                    ");
+
+            bool done = false;
+            int total = 0;
+
+            string expr = "";
+            int number = 0;
+
+            do {
+                Console.WriteLine("Total: " + total);
+                Console.WriteLine("Enter Expression:");
+                expr = Console.ReadLine();
+                if(expr[0] != '='){number = int.Parse(expr.Substring(1));}
+                switch (expr[0]) {
+                    case '+': total += number; break;
+                    case '-': total -= number; break;
+                    case '*': total *= number; break;
+                    case '/': total /= number; break;
+                    case '=': done = true; break;
+                    default: Console.WriteLine("\n\nInvalid Operator!\n\n"); break;
+                };
+            } while (!done);
+
+            Console.WriteLine("\nFinal Total: " + total);
+        }
+    }
+}
